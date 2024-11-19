800076aea0e0a028016fde73fa42a7294a309980
Mert Karahan <mertkarahan955@gmail.com>
2024-11-20 00:30:34 +0300

    ci/cd user pool fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login.tsx";
 import Home from "./components/Home/Home.tsx"; // Create a simple Home component
 import { UserProvider } from "./contexts/UserContext.tsx";
-
-import { Authenticator } from "@aws-amplify/ui-react";
 export default function App() {
   return (
-    <Authenticator>
     <UserProvider>
     <Router>
       <Routes>
@@ -16,6 +13,5 @@ export default function App() {
       </Routes>
     </Router>
     </UserProvider>
-    </Authenticator>
   );
 }
