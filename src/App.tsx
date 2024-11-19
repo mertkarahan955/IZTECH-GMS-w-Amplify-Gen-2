65380c2a48ecad3c1605e3dbc874129c09be655a
Mert Karahan <mertkarahan955@gmail.com>
2024-11-20 00:13:46 +0300

    testing deployed userpool

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login.tsx";
 import Home from "./components/Home/Home.tsx"; // Create a simple Home component
 import { UserProvider } from "./contexts/UserContext.tsx";
+
+import { Authenticator } from "@aws-amplify/ui-react";
 export default function App() {
   return (
+    <Authenticator>
     <UserProvider>
     <Router>
       <Routes>
@@ -13,5 +16,6 @@ export default function App() {
       </Routes>
     </Router>
     </UserProvider>
+    </Authenticator>
   );
 }
